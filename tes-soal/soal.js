69a0363fd6fabd1a36e9bd25964f116a255a5b5f
dila <lilafadila64@gmail.com>
2022-11-27 22:33:19 +0800

    add noted

--- tes-soal/soal.js
+++ tes-soal/soal.js
@@ -1,61 +1,56 @@
-// Fiz Buz
-// ide : 3 = fizz, 5 = buzz, 3 & 5 = fizzBuzz
-// [1, 2, fizz, 4, buzz, ---- 14, fizzBuzz]
+// Soal 1 (Fizz Buzz)
 function fizzBuzz(num) {
   let temp = [];
   for (let i = 1; i <= num; i++) {
     // console.log(i);
     if (i % 3 === 0 && i % 5 === 0) {
+      // cari angka yg habis dibagi 3 & 5
       temp.push("fizzBuzz");
     } else if (i % 5 === 0) {
+      // cari angka yg habis dibagi 5
       temp.push("buzz");
     } else if (i % 3 === 0) {
+      // cari angka yg habis dibagi 3
       temp.push("fizz");
     } else {
+      // angka selain fizz buzz
       temp.push(i);
     }
   }
-  //   console.log(temp);
   return temp;
 }
 
 console.log(fizzBuzz(15));
-// [1, 2, 3 ---- 15]
 
-// reverse / balik kata
+// soal 2 (reverse / balik kata)
 
 function reverse(string) {
   let balikan = "";
   let number = "";
 
   for (let i = string.length - 1; i >= 0; i--) {
-    // balikan += string[i];
     // perkondisian. => deteksi mana number mana string
     if (+string[i]) {
+      // ubah string menjadi number
       console.log(string[i]);
-      number += string[i];
+      number += string[i]; // tampung number
     } else {
-      balikan += string[i];
+      balikan += string[i]; //tampung khusus alphabet (huruf)
     }
   }
-  console.log(number);
-  console.log(balikan);
+
   return number + balikan;
 }
 
 console.log(reverse("NEG1IE"));
-// 1EIGEN
-let stringAngka = "123";
 
-// kata terpanjang
-// aku cayang mantannn
-
+// soal 3 (deteksi kata terpanjang)
 function longestWord(string) {
   let word = string.split(" ");
   let longest = 0; //3 > 6 > 8
   for (let i = 0; i < word.length; i++) {
-    console.log(word[i]);
     if (word[i].length > longest) {
+      // cari panjang Word yang paling panjang
       longest = word[i].length;
     }
   }
@@ -64,4 +59,3 @@ function longestWord(string) {
 
 console.log(longestWord("aku cayang mantannn"));
 console.log(longestWord("mantan ngajak balikan hahahhahaha"));
-// ['aku', 'cayang', 'mantannn']
